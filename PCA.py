418f0fa982bb550f044a82893bc37eb2b4e3e8a7
percy212 <49874001+percy212@users.noreply.github.com>
2019-04-26 11:16:10 +0900

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 def PCA(A,t):
+    m, n = A.shape
+    
+    if t > n :
+        print("You Entered Higher Dimension")
+        print("Returned Zero")
+        return 0
+    
     A_cen = A - A.mean(axis=0)
 
     U, D ,Vt = np.linalg.svd(A_cen)
