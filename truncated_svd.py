dca4b9ee869d428ac491944a2d54a3d93873bc69
percy212 <49874001+percy212@users.noreply.github.com>
2019-05-03 15:58:17 +0900

    Update truncated_svd.py

--- truncated_svd.py
+++ truncated_svd.py
@@ -1,6 +1,29 @@
 import numpy as np
 
 def truncated_svd(a,t):
+    """ Returns truncatedSVD result matrices
+
+
+    When t is larger than original dimension,
+    return zeros
+
+    
+    Parameters
+    ---------
+    a : numpy.ndarray (2D)
+        matrix to be decomposed
+    t : int
+        dimension to reduce
+
+    Returns
+    ---------
+    trunc_u : numpy.ndarray (2D)
+        truncated u matrix
+    trunc_s : numpy.ndarray (2D)
+        truncated s matrix
+    trunc_vt : numpy.ndarray (2D)
+        truncated vt matrix
+    """
     m, n = a.shape
     u, s, vt = np.linalg.svd(a)
     trunc_u = np.zeros((m,t))
@@ -15,4 +38,4 @@ def truncated_svd(a,t):
         print('You Entered Wrong t Value')
         return 0, 0, 0
     else:
-        return trunc_U, trunc_D, trunc_Vt
+        return trunc_u, trunc_s, trunc_vt
