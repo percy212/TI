a2173cbb00ee2d8faab142c94ee9e9aba9882a00
percy212 <49874001+percy212@users.noreply.github.com>
2019-05-03 16:12:28 +0900

    Update pca.py

--- pca.py
+++ pca.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 def pca(a,t):
+    """ Returns PCA result matrix
+
+    When t is larger than original dimension,
+    returns zero
+
+    Parameters
+    ---------
+    a : numpy.ndarray (2D)
+        matrix to be analyzed
+    t : int
+        reduced dimension
+
+    Returns
+    ---------
+    pcv : numpy.ndarray (2D)
+        result of PCA
+
+    """
     m, n = a.shape    
     a_cen = a - a.mean(axis=0)
     u, s, vt = np.linalg.svd(a_cen)
